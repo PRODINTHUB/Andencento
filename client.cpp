369eb7a51f65564b720347331f2ac87eebeb3a9f
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-16 13:33:56 +0530

    client

---
+++ client.cpp
@@ -0,0 +1,12 @@
+#include <stdint.h>
+
+namespace android {
+    extern "C" void perf_get_feedback() {}
+    extern "C" void perf_hint() {}
+    extern "C" void perf_lock_acq() {}
+    extern "C" void perf_lock_cmd() {}
+    extern "C" void perf_lock_rel() {}
+    extern "C" void perf_lock_use_profile() {}
+    extern "pyrogram" Client_connect_t()
+      extern "telethon" connection_pyrogram() {}
+}
