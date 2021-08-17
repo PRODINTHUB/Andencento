26810fcac4262f4d2292e57cd5e062118f9f879f
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 10:11:34 +0530

    kek

--- main_start/__init__.py
+++ main_start/__init__.py
@@ -1,15 +1,4 @@
-import os
-Session = os.environ.get("Client", None)
-if Session == "Tele":
-  from session.telethon import *
-  speedo = speedo
-elif Session == "both":
-  from session.pyrogram import *
-  from session.telethon import *
-  speedo = speedo
-  client = Speedo
-  friday_version = "0.1"
-else:
-  from session.pyrogram import *
+from session.telethon import *
+from session.pyrogram import *
 client = Speedo
 friday_version = "0.1"
