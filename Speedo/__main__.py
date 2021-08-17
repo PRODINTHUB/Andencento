66c42f02549b0f261cd1ea7f37144fe022f6799a
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 14:36:11 +0530

    logs

--- Speedo/__main__.py
+++ Speedo/__main__.py
@@ -11,7 +11,32 @@ from .utils import start_assistant
 from pathlib import Path
 import asyncio
 import telethon.utils
-from Speedo import LOGS
+
+from logging import basicConfig, getLogger, INFO, DEBUG
+if bool(ENV):
+    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
+
+    if CONSOLE_LOGGER_VERBOSE:
+        basicConfig(
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=DEBUG,
+        )
+    else:
+        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+                    level=INFO)
+    LOGS = getLogger(__name__)
+
+    # Check if the config was edited by using the already used variable.
+    # Basically, its the 'virginity check' for the config file ;)
+    CONFIG_CHECK = os.environ.get(
+        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
+
+    if CONFIG_CHECK:
+        LOGS.info(
+            "Please remove the line mentioned in the first hashtag from the config.env file"
+        )
+        quit(1)
+
 
 
                     
