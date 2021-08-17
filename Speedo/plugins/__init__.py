4b394f93d7748690d48a3f4a88024bf3985954d0
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 15:31:17 +0530

    bestie

--- Speedo/plugins/__init__.py
+++ Speedo/plugins/__init__.py
@@ -3,3 +3,7 @@ import os
 import asyncio
 from telethon import TelegramClient
 from telethon.sessions import StringSession
+from .. import *
+from ..utils import *
+from ..config import *
+from ..variables import *
