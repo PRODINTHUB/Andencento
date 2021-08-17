7110b0a1dffd7660fd2388fe96a5bde9e59ba74f
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 14:05:10 +0530

    config

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ class Var(object):
         except ValueError:
             raise ValueError(
                 "Invalid Private Group ID. Make sure your ID is starts with -100 and make sure that it is only numbers.")
-class Development(Var):
+class Development(Config):
     LOGGER = True
     # Here for later purposes
 ENV = bool(os.environ.get("ENV", False))
