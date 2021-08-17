1395aed3b5bced9d06369a2ea5fc3a6a6174c827
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 09:57:57 +0530

    kk

--- main_start/__main__.py
+++ main_start/__main__.py
@@ -6,13 +6,12 @@ if Session == "Pyro":
     await run_bot()
     Speedo.loop.run_until_complete(run_bot())
 elif Session == "both":
-  async def impro(): 
-    from session.telethon_main import *
-    await mongo_check()
-    await run_bot()
-    Speedo.loop.run_until_complete(run_bot())
-    speedo.loop.run_until_complete(run_bot())
-    print("Starting Telethon && pyrogram")
+  print("Starting Telethon && pyrogram")
+  from session.telethon_main import *
+  await mongo_check()
+  await run_bot()
+  Speedo.loop.run_until_complete(run_bot())
+  speedo.loop.run_until_complete(run_bot())
 else:
   from session.telethon_main import *
   await speedo.start()
