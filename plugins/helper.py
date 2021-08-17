e840fae038d088b16f662894902e1843bff6587e
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 19:36:45 +0530

    helkp

--- plugins/helper.py
+++ plugins/helper.py
@@ -1,1 +1,52 @@
 
+@bot.on(Speedo_cmd(pattern="help ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
+async def yardim(event):
+    if event.fwd_from:
+        return
+    tgbotusername = Config.BOT_USERNAME
+    input_str = event.pattern_match.group(1)
+    try:
+        if not input_str == "":
+            if input_str in CMD_HELP:
+                await eor(event, str(CMD_HELP[args]))
+    except:
+        pass
+    if tgbotusername is not None:
+        results = await event.client.inline_query(tgbotusername, "Speedo_help")
+        await results[0].click(
+            event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+        )
+        await event.delete()
+    else:
+        await eor(event, "**⚠️ ERROR !!** \nPlease Re-Check BOT_TOKEN & BOT_USERNAME on Heroku.")
+
+
+@bot.on(Speedo_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
+async def Speedot(event):
+    if event.fwd_from:
+        return
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await eor(event, str(CMD_HELP[args]))
+        else:
+            await eod(event, "**⚠️ Error !** \nNeed a module name to show plugin info.")
+    else:
+        string = ""
+        sayfa = [
+            sorted(list(CMD_HELP))[i : i + 5]
+            for i in range(0, len(sorted(list(CMD_HELP))), 5)
+        ]
+
+        for i in sayfa:
+            string += f"`▶️ `"
+            for sira, a in enumerate(i):
+                string += "`" + str(a)
+                if sira == i.index(i[-1]):
+                    string += "`"
+                else:
+                    string += "`, "
+            string += "\n"
+        await eod(event, "Please Specify A Module Name Of Which You Want Info" + "\n\n" + string)
