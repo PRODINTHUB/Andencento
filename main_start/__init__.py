bd27be8005e5fe9506bf9a1c2a4016701b29b14c
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 09:44:56 +0530

    ll

--- main_start/__init__.py
+++ main_start/__init__.py
@@ -2,8 +2,14 @@ import os
 Session = os.environ.get("Client", None)
 if Session == "Tele":
   from session.telethon import *
+  speedo = speedo
+elif Session == "both":
+  from session.pyrogram import *
+  from session.telethon import *
+  speedo = speedo
+  client = Speedo
+  friday_version = "0.1"
 else:
   from session.pyrogram import *
-speedo = speedo
 client = Speedo
 friday_version = "0.1"
