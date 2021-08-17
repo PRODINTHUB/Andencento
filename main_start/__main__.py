393b0354eb3b3928314527c0e9865109be64a525
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 09:00:01 +0530

    main

--- main_start/__main__.py
+++ main_start/__main__.py
@@ -1,7 +1,8 @@
 from session.pyrogram_main import *
-async def op():
-  await mongo_check()
-  await run_bot()
-  
-if __name__ == "__main__":
-  Speedo.loop.run_until_complete(run_bot())
+if Session == "Pyro" or "Both":
+  async def op():
+    await mongo_check()
+    await run_bot()
+    Speedo.loop.run_until_complete(run_bot())
+else:
+  return
