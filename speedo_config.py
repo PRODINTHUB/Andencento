e1e0fc4ce8fe0b1152ac85d07924a19717c278ca
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 17:06:10 +0530

    ,

--- speedo_config.py
+++ speedo_config.py
@@ -20,7 +20,7 @@ class Config(object):
         send_messages=True
     )
     API_HASH = os.environ.get("API_HASH", None)
-    APP_ID = os.environ.get("APP_ID", None)
+    APP_ID = os.environ.get("API_ID", None)
     AUTH_TOKEN_DATA = os.environ.get("AUTH_TOKEN_DATA", None)
     if AUTH_TOKEN_DATA != None:
         os.makedirs(TMP_DOWNLOAD_DIRECTORY)
