5f3705761d728509000ee18a883c9745ec6873da
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 10:10:34 +0530

    k

--- main_start/__main__.py
+++ main_start/__main__.py
@@ -1,24 +1,7 @@
 from session.pyrogram_main import *
-Session = os.environ.get("Client", None)
-if Session == "Pyro":
-  async def op():
-    await mongo_check()
-    await run_bot()
-    Speedo.loop.run_until_complete(run_bot())
-elif Session == "both":
-  
-  print("Starting Telethon && pyrogram")
-  from session.telethon_main import *
-  async def kk():
-    await mongo_check()
-    await run_bot()
-    Speedo.loop.run_until_complete(run_bot())
-    speedo.loop.run_until_complete(run_bot())
-else:
-  from session.telethon_main import *
-  async def impro(): 
-   await speedo.start()
-
+async def op():
+  await mongo_check()
+  await run_bot()
 
 if __name__ == "__main__":
-  speedo.loop.run_until_complete(run_bot()) or Speedo.loop.run_until_complete(kk()) or speedo.loop.run_until_complete(impro())
+  Speedo.loop.run_until_complete(run_bot())
