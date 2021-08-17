cbdce797257dd5a0329f2e1ec672868394976dc2
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 18:03:35 +0530

    k

--- Speedo/utils/plug.py
+++ Speedo/utils/plug.py
@@ -51,15 +51,15 @@ def load_module(shortname):
         name = "Speedo.plugins.{}".format(shortname)
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        mod.bot = bot
-        mod.tgbot = bot.tgbot
+        mod.bot = speedo
+        mod.tgbot = speedo.tgbot
         mod.command = command
         mod.logger = logging.getLogger(shortname)
         # support for uniborg
         sys.modules["uniborg.util"] = Speedo.utils
         mod.Config = Config
-        mod.borg = bot
-        mod.Speedo = bot
+        mod.borg = speedo
+        mod.Speedo = speedo
         mod.edit_or_reply = edit_or_reply
         mod.eor = edit_or_reply
         mod.delete_speedo = delete_speedo
