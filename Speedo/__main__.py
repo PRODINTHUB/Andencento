ef329f2e513806531a3f8432252cfcf644f0a0b5
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 14:39:58 +0530

    ikmpro

--- Speedo/__main__.py
+++ Speedo/__main__.py
@@ -13,30 +13,7 @@ import asyncio
 import telethon.utils
 
 from logging import basicConfig, getLogger, INFO, DEBUG
-if bool(ENV):
-    CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
-
-    if CONSOLE_LOGGER_VERBOSE:
-        basicConfig(
-            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-            level=DEBUG,
-        )
-    else:
-        basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-                    level=INFO)
-    LOGS = getLogger(__name__)
-
-    # Check if the config was edited by using the already used variable.
-    # Basically, its the 'virginity check' for the config file ;)
-    CONFIG_CHECK = os.environ.get(
-        "___________PLOX_______REMOVE_____THIS_____LINE__________", None)
-
-    if CONFIG_CHECK:
-        LOGS.info(
-            "Please remove the line mentioned in the first hashtag from the config.env file"
-        )
-        quit(1)
-
+LOGS = getLogger(__name__)
 
 
                     
@@ -82,10 +59,12 @@ files = glob.glob(path)
 for name in files:
     try:
         with open(name) as f:
+            LOGS = getLogger(__name__)
             path1 = Path(f.name)
             shortname = path1.stem
             load_module(shortname.replace(".py", ""))
     except Exception:
+        LOGS = getLogger(__name__)
         LOGS.info(f"[Speedo] - Official - ERROR - {shortname}")
         LOGS.info(str(traceback.print_exc()))
 
