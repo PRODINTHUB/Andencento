8f23b8d3922de63a43803ec4bd9f10864eb1b201
Developer <72003733+InternetAmethyst@users.noreply.github.com>
2021-08-17 15:56:38 +0530

    Speedo

--- Speedo/plugins/__init__.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import os
-import asyncio
-from telethon import TelegramClient
-from telethon.sessions import StringSession
-from .. import *
-from ..utils import *
-from ..config import *
-from ..variables import *
