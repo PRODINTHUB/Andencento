3c100e6b00ad9ab357adbcf658f1fb9f2a5fd11e
PRODINTHUB <105144942+PRODINTHUB@users.noreply.github.com>
2022-05-24 13:24:25 +0530

    Int

--- ex_config.py
+++ ex_config.py
@@ -1,6 +1,6 @@
 # EDIT THIS FILE AND RENAME TO config.py TO MAKE THIS BOT WORKING
 # FILL THESE VALUES ACCORDINGLY.
-
+# Only For Telethon
 from speedo_config import Config
 
 class Development(Config):
@@ -10,7 +10,7 @@ class Development(Config):
 
   # the name to display in your alive message.
   # If not filled anything then default value is I'm Hêll.
-  YOUR_NAME = "I'm Hêll"
+  YOUR_NAME = "Your Value"
 
   # create any PostgreSQL database.
   # I recommend to use elephantsql and paste that link here
